0e46edf8bddb51e0392390ab5b2e28fc3bbadd2f
unknown <nial2400@student.miun.se>
2026-02-03 14:40:16 +0100

    Added backend template

---
+++ backend/src/main/java/com/dt170g/g3/backend/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.dt170g.g3.backend;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
