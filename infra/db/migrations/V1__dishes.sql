b6c3efd83720897a8f2f5e50cd6a098557577b2b
Axel Friman <axelfriman04@gmail.com>
2026-02-09 14:54:21 +0100

    Added 7 tables

--- infra/db/migrations/V1__dishes.sql
+++ infra/db/migrations/V1__dishes.sql
@@ -1,3 +1,4 @@
+--Main Tables
 CREATE TABLE dish (
     id INT AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
@@ -8,4 +9,31 @@ CREATE TABLE dish (
 CREATE TABLE category (
     id INT AUTO_INCREMENT,
     name VARCHAR(255)
+) ENGINE=InnoDB;
+
+CREATE TABLE carte_menu(
+    id INT AUTO_INCREMENT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
+
+CREATE TABLE lunch_menu(
+    id INT AUTO_INCREMENT,
+    date DATE DEFAULT CURRENT_DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
+
+--Junction Tables
+CREATE TABLE dish_category(
+    dish_id INT NOT NULL,
+    category_id INT NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE dish_carte_menu(
+    dish_id INT NOT NULL,
+    carte_menu_id INT NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE dish_lunch_menu(
+    dish_id INT NOT NULL,
+    lunch_menu_id INT NOT NULL
 ) ENGINE=InnoDB;
