89f154d4d4ff68800a3b87d1d2ef1f763af13e69
Axel Friman <axelfriman04@gmail.com>
2026-02-11 16:15:47 +0100

    Removed unneccesary imports

--- backend/src/main/java/com/dt170g/g3/backend/api/DishApi.java
+++ backend/src/main/java/com/dt170g/g3/backend/api/DishApi.java
@@ -9,12 +9,6 @@ import jakarta.ws.rs.core.MediaType;
 import com.dt170g.g3.backend.entities.Dish;
 import java.util.List;
 
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.Consumes;
-
-
-
 @Path("/dish")
 public class DishApi {
     @Inject
