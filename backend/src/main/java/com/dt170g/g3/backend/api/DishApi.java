728eac8b9dbabadf56d0a6caa79593e169c51072
Axel Friman <axelfriman04@gmail.com>
2026-02-11 15:35:57 +0100

    Added API functions for dish

---
+++ backend/src/main/java/com/dt170g/g3/backend/api/DishApi.java
@@ -0,0 +1,37 @@
+package com.dt170g.g3.backend;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import com.dt170g.g3.backend.entities.Dish;
+import java.util.List;
+
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.Consumes;
+
+
+
+@Path("/dish")
+public class DishApi {
+    @Inject
+    private DatabaseHandler dbHandler;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Dish> getAllDishes() {
+        return dbHandler.getDishes();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Dish getDishById(@PathParam("id") int id) {
+        return dbHandler.getDishById(id);
+    }
+
+
+}
