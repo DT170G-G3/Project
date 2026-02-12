e9c7d30ee24dbec3b09cfe3e01b1bf52613b757d
Axel Friman <axelfriman04@gmail.com>
2026-02-12 13:18:29 +0100

    Added API request for lunchmenu

---
+++ backend/src/main/java/com/dt170g/g3/backend/api/MenuApi.java
@@ -0,0 +1,33 @@
+package com.dt170g.g3.backend;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import com.dt170g.g3.backend.entities.LunchMenu;
+import com.dt170g.g3.backend.entities.Dish;
+import java.util.List;
+
+
+
+@Path("/menu")
+public class MenuApi {
+    @Inject
+    private LunchMenuHandler lunchHandler;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Dish> getAllDishes() {
+        return lunchHandler.getDishesToday();
+    }
+
+    @GET
+    @Path("/lunch")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Dish> getLunchMenu() {
+        return lunchHandler.getDishesToday(); }
+
+
+}
--- backend/src/main/java/com/dt170g/g3/backend/api/RestApplication.java
+++ backend/src/main/java/com/dt170g/g3/backend/api/RestApplication.java
@@ -1,3 +1,14 @@
+/**
+ * RestApplication.java
+ *
+ * Configures the JAX-RS application for this backend.
+ * Sets the base path for all REST endpoints to "/api".
+ *
+ * By extending jakarta.ws.rs.core.Application, this class
+ * serves as the entry point for the REST API.
+ */
+
+
 package com.dt170g.g3.backend;
 
 import jakarta.ws.rs.ApplicationPath;
