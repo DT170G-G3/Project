53241073095b144a6bc8eb3424937ffe5a776611
Slim7Pro <sigrid.elofsson@gmail.com>
2026-02-15 18:51:27 +0100

    lista med test-ordrar och checkbox adderat i varje kolumn

--- android-app-2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ android-app-2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +21,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        RecyclerView starterRecyclerView = findViewById(R.id.starterRecyclerView);
+        RecyclerView mainRecyclerView = findViewById(R.id.mainCourseRecyclerView);
+        RecyclerView dessertRecyclerView = findViewById(R.id.dessertRecyclerView);
+
+        starterRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mainRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        dessertRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        //bara för test
+        List<Order> starterOrder = Arrays.asList(
+                new Order(1, Arrays.asList("Carpaccio")),
+                        new Order(5, Arrays.asList("Caprese"))
+        );
+
+        List<Order> mainOrder = Arrays.asList(
+                new Order(2, Arrays.asList("kött", "Fish and chips")),
+                new Order(6, Arrays.asList("Bolognese"))
+        );
+
+        List<Order> dessertOrder = Arrays.asList(
+                new Order(3, Arrays.asList("kladdkaka", "glass")),
+                new Order(1, Arrays.asList("tiramisu"))
+        );
+
+        starterRecyclerView.setAdapter(new OrdersAdapter(starterOrder));
+        mainRecyclerView.setAdapter(new OrdersAdapter(mainOrder));
+        dessertRecyclerView.setAdapter(new OrdersAdapter(dessertOrder));
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ android-app-2/app/src/main/java/com/example/myapplication/OrdersAdapter.java
@@ -0,0 +1,59 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class OrdersAdapter extends RecyclerView.Adapter<OrdersAdapter.OrderViewHolder> {
+
+    private List<Order> orderList;
+
+    public OrdersAdapter(List<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+    @NonNull
+    @Override
+    public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.each_order, parent, false);
+        return new OrderViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrderViewHolder holder, int position) {
+        Order order = orderList.get(position);
+        holder.tableTextView.setText("Bord " + order.getTableNumber());
+
+        holder.listOfDishesLinearLayout.removeAllViews();
+        for(String o : order.getOrders()) {
+            TextView textView = new TextView(holder.itemView.getContext());
+            textView.setText(o);
+            holder.listOfDishesLinearLayout.addView(textView);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return orderList.size();
+    }
+
+    static class OrderViewHolder extends RecyclerView.ViewHolder {
+        TextView tableTextView;
+        LinearLayout listOfDishesLinearLayout;
+        CheckBox completeOrderCheckBox;
+        public OrderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tableTextView = itemView.findViewById(R.id.tableTextView);
+            listOfDishesLinearLayout = itemView.findViewById(R.id.listOfDishesLinearLayout);
+            completeOrderCheckBox = itemView.findViewById(R.id.completeOrderCheckBox);
+        }
+    }
+}
