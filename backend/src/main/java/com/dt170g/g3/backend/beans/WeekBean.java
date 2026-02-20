6de052f48f2834d38efa42406468d67d59b51c2c
Jesper <jesper.elovsson92@gmail.com>
2026-02-20 14:35:36 +0100

    Week logic for Admin

--- backend/src/main/java/com/dt170g/g3/backend/beans/WeekBean.java
+++ backend/src/main/java/com/dt170g/g3/backend/beans/WeekBean.java
@@ -1,19 +1,67 @@
 package com.dt170g.g3.backend.beans;
 
-import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
-
+import java.io.Serializable;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.IsoFields;
+import java.time.temporal.WeekFields;
+import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * The week-bean is used to calculate the current week and
+ * the days of a week. These functions can then be used by the admin page.
+ * 
+ */
 @Named("week")
-@ApplicationScoped
-public class WeekBean {
-    private LocalDate weekStart;
-    private List<LocalDate> weekdays;
+@ViewScoped
+public class WeekBean implements Serializable {
+    private int weekNumber;
 
-    public int getCurrentWeekNumber(){
-        return 1;
+    @PostConstruct
+    public void init() {
+        setWeekNumber(LocalDate.now().get(IsoFields.WEEK_OF_WEEK_BASED_YEAR));
+    }
+
+    public void setWeekNumber(int num){
+        this.weekNumber = num;
+    }
+
+    public int getWeekNumber(){
+        return weekNumber;
+    }
+
+    /*
+     * Used to get current week and upcoming two weeks week number.
+     * Applied to the buttons in the admin view.
+     */
+    public List<Integer> getWeekNumbers(){
+        int weekNumber = LocalDate.now().get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
+        List<Integer> weekNumbers = new ArrayList<>();
+        for(int i = 0; i < 3; i++){
+            weekNumbers.add(weekNumber + i);
+        }
+        return weekNumbers;
+    }
+    /* To recives the dates of mon-fri for any given weeknumber */
+    private List<LocalDate> getDaysOfWeek(int week){
+        WeekFields weekFields = WeekFields.ISO;
+        LocalDate startOfWeek = LocalDate.now()
+                .withYear(LocalDate.now().getYear())
+                .with(weekFields.weekOfYear(),week)
+                .with(DayOfWeek.MONDAY);
+        List<LocalDate> weekdays = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            weekdays.add(startOfWeek.plusDays(i));
+        }
+        return weekdays;
+    }
+    /* The method used by the facelet. */
+    public List<LocalDate> getDaysOfWeek(){
+        return getDaysOfWeek(weekNumber);
     }
 
 }
