4ed26fecf17a69121669a900b16002e130d0c35b
Emma <emvi2304@student.miun.se>
2026-02-22 19:39:15 +0100

    Ändring av namn på filer

--- android-app-1/app/src/main/java/com/DT170G/G3/android_app_1/MainActivity.java
+++ android-app-1/app/src/main/java/com/DT170G/G3/android_app_1/MainActivity.java
@@ -18,8 +18,25 @@ import com.DT170G.G3.android_app_1.classes.PagerAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+//Temporära imports
+import com.DT170G.G3.android_app_1.drinks.DrinksRepository;
+import com.DT170G.G3.android_app_1.orders.OrdersRepository;
+import com.DT170G.G3.android_app_1.tables.TablesRepository;
+import com.DT170G.G3.android_app_1.dishes.DishesRepository;
+import com.DT170G.G3.android_app_1.dishes.Dish;
+import com.DT170G.G3.android_app_1.tables.Table;
+import com.DT170G.G3.android_app_1.drinks.Drink;
+import com.DT170G.G3.android_app_1.orders.Order;
+import java.util.List;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Temporärt test
+    DishesRepository dishesRepo = new DishesRepository();
+    DrinksRepository drinksRepo = new DrinksRepository();
+    TablesRepository tablesRepo = new TablesRepository();
+    OrdersRepository ordersRepo = new OrdersRepository();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +52,17 @@ public class MainActivity extends AppCompatActivity {
 
         changeTabListener();
         sendOrderButtonListener();
+
+
+        //------GET--------
+        asyncLoadTables();
+        asyncLoadDrinks();
+        asyncLoadDishes();
+        //------/GET--------
     }
 
     public void sendOrderButtonListener(){
-        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        //ViewPager2 viewPager = findViewById(R.id.viewPager);
         BottomNavigationView bottomNavigationMenu = findViewById(R.id.bottomNavigationMenu);
 
         //Resest form - ändra så att den skickar till databasen oxå
@@ -143,6 +167,164 @@ public class MainActivity extends AppCompatActivity {
                                                }
         );
 
+    }
+
+    private void asyncLoadDishes() {
+        dishesRepo.getDishes(new DishesRepository.GetCallback() {
+            @Override
+            public void onSuccess(List<Dish> dishes) {
+                //DishesCache.setCache(dishes);   //om vi ska använda cache
+                populateDishesUI(dishes);
+            }
+            @Override
+            public void onError(String message) {
+                Log.e("DISHES", "Fel: " + message);
+            }
+        });
+    }
+    private void asyncLoadTables() {
+        tablesRepo.getTables(new TablesRepository.GetCallback() {
+            @Override
+            public void onSuccess(List<Table> tables) {
+                populateTablesUI(tables);
+            }
+            @Override
+            public void onError(String message) {
+                Log.e("TABLES", "Fel: " + message);
+            }
+        });
+    }
+    private void asyncLoadDrinks() {
+        drinksRepo.getDrinks(new DrinksRepository.GetCallback() {
+            @Override
+            public void onSuccess(List<Drink> drinks) {
+                populateDrinksUI(drinks);
+            }
+            @Override
+            public void onError(String message) {
+                Log.e("DRINKS", "Fel: " + message);
+            }
+        });
+    }
+
+    private void populateTablesUI(List<Table> tables) {
+        // Your code here
+        Log.d("TABLE", "Size: " +tables.size());
+
+    }
+    private void populateDrinksUI(List<Drink> drinks) {
+        // Your code here
+        Log.d("DRINK", "Size: " +drinks.size());
+    }
+    public void populateDishesUI(List<Dish> dishes) {
+        // Your UI code
+        Log.d("DISH", "Size: " +dishes.size());
+    }
+
+
+
+    // Methods used for testing below
+    private void createOrder() {
+        Order first = new Order();
+        first.note = "Extra salt";
+        first.tableId = 3;
+
+        // First order item and its category
+        Order.OrderItem item = new Order.OrderItem();
+        item.dishId = 1;    // id of dish ordered?
+        item.category = 2;  // appetizer, main course, dessert?
+
+
+        // Second order item and its category
+        Order.OrderItem item2 = new Order.OrderItem();
+        item2.dishId = 1;
+        item2.category = 2;
+
+        //Third order item and its category
+        Order.OrderItem item3 = new Order.OrderItem();
+        item3.dishId = 2;
+        item3.category = 2;
+
+        //Creates a list of the items ordered
+        first.orderedItems = java.util.Arrays.asList(item, item2, item3);
+
+        //asynchronous post the order to the json-server ( later database)
+        ordersRepo.postOrder(first, new OrdersRepository.PostCallback() {
+            @Override
+            public void onSuccess(Order postOrder) {
+                Log.d("ORDER", "SUCCESSFULLY POSTED ORDER");
+            }
+            @Override
+            public void onError(String message) {
+                Log.e("ORDER", "FAILED TO POST: " + message);
+            }
+        });
+    }
+    private void createOrder2() {
+        Order first = new Order();
+        first.note = "Ingen lök";
+        first.tableId = 5;
+
+        // First order item and its category
+        Order.OrderItem item = new Order.OrderItem();
+        item.dishId = 3;    // id of dish ordered?
+        item.category = 1;  // appetizer, main course, dessert?
+
+        // Second order item and its category
+        Order.OrderItem item2 = new Order.OrderItem();
+        item2.dishId = 5;
+        item2.category = 2;
+
+        //Third order item and its category
+        Order.OrderItem item3 = new Order.OrderItem();
+        item3.dishId = 4;
+        item3.category = 2;
+        first.orderedItems = java.util.Arrays.asList(item, item2, item3);
+
+        //asynchronous post the order to the json-server ( later database)
+        ordersRepo.postOrder(first, new OrdersRepository.PostCallback() {
+            @Override
+            public void onSuccess(Order postOrder) {
+                Log.d("ORDER", "SUCCESSFULLY POSTED ORDER");
+            }
+            @Override
+            public void onError(String message) {
+                Log.e("ORDER", "FAILED TO POST: " + message);
+            }
+        });
+    }
+    private void createOrder3() {
+        Order first = new Order();
+        first.note = "Extra allt";
+        first.tableId = 2;
+
+        // First order item and its category
+        Order.OrderItem item = new Order.OrderItem();
+        item.dishId = 2;    // id of dish ordered?
+        item.category = 2;  // appetizer, main course, dessert?
+
+        // Second order item and its category
+        Order.OrderItem item2 = new Order.OrderItem();
+        item2.dishId = 1;
+        item2.category = 2;
+
+        //Third order item and its category
+        Order.OrderItem item3 = new Order.OrderItem();
+        item3.dishId = 3;
+        item3.category = 3;
+        first.orderedItems = java.util.Arrays.asList(item, item2, item3);
+
+        //asynchronous post the order to the json-server ( later database)
+        ordersRepo.postOrder(first, new OrdersRepository.PostCallback() {
+            @Override
+            public void onSuccess(Order postOrder) {
+                Log.d("ORDER", "SUCCESSFULLY POSTED ORDER");
+            }
+            @Override
+            public void onError(String message) {
+                Log.e("ORDER", "FAILED TO POST: " + message);
+            }
+        });
     }
 
 
--- android-app-3/app/build.gradle.kts
+++ android-app-3/app/build.gradle.kts
@@ -3,13 +3,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.DT170G.G3.android_app_3"
     compileSdk {
         version = release(36)
     }
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.DT170G.G3.android_app_3"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.viewpager2)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
