780236999669232319b604a0337baccd7d2a8dd9
Emma <emvi2304@student.miun.se>
2026-02-24 10:00:01 +0100

    Lagt till så varmrätter hämtas från API

--- android-app-1/app/src/main/java/com/DT170G/G3/android_app_1/dishes/Dish.java
+++ android-app-1/app/src/main/java/com/DT170G/G3/android_app_1/dishes/Dish.java
@@ -3,7 +3,15 @@ package com.DT170G.G3.android_app_1.dishes;
 public class Dish {
     public Category category;
     public String description;
-    public int id;
+    public int categoryId;
     public String name;
     public double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
 }
