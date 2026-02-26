861129f8038c7d60d4104e756d4460b55a3f93a8
MikeGadget <mija2306@student.miun.se>
2026-02-26 10:56:00 +0100

    Added example of how to POST an Order.

--- android-app-1/app/src/main/java/com/DT170G/G3/android_app_1/MainActivity.java
+++ android-app-1/app/src/main/java/com/DT170G/G3/android_app_1/MainActivity.java
@@ -27,6 +27,7 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.DT170G.G3.android_app_1.classes.PagerAdapter;
+import com.DT170G.G3.android_app_1.dishes.Category;
 import com.DT170G.G3.android_app_1.dishes.Dish;
 import com.DT170G.G3.android_app_1.dishes.DishesRepository;
 import com.DT170G.G3.android_app_1.drinks.Drink;
@@ -38,6 +39,8 @@ import com.DT170G.G3.android_app_1.tables.Table;
 import com.DT170G.G3.android_app_1.tables.TablesRepository;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -61,6 +64,10 @@ public class MainActivity extends AppCompatActivity {
         sendOrderButtonListener();
 
 
+        //------POST--------
+        //asyncCreateOrder(order);
+        //-----/POST--------
+
         //-------GET--------
         //asyncLoadTables();
         asyncLoadOrders();
@@ -68,8 +75,7 @@ public class MainActivity extends AppCompatActivity {
         //asyncLoadDrinks();
         //------/GET--------
 
-        //------POST--------
-        //-----/POST--------
+
     }
 
     public void sendOrderButtonListener(){
@@ -219,6 +225,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void asyncCreateOrder(Order order) {
+        //asynchronous post the order to the database
+        ordersRepo.postOrder(order, new OrdersRepository.PostCallback() {
+            @Override
+            public void onSuccess(Order postOrder) {
+                Log.d("ORDER", "SUCCESSFULLY POSTED ORDER");
+            }
+            @Override
+            public void onError(String message) {
+                Log.e("ORDER", "FAILED TO POST: " + message);
+            }
+        });
+    }
+
+
+
+
 
     private void populateTablesUI(List<Table> tables) {
         // Your code here
@@ -226,9 +249,10 @@ public class MainActivity extends AppCompatActivity {
     }
     private void populateOrdersUI(List<Order> orders) {
         // Your code here
-        //Log.d("ORDER", "TOTAL ORDERS: " +orders.size());
-        for (int i = 0; i < 5; i++) {
+        Log.d("ORDER", "TOTAL ORDERS: " +orders.size());
+        for (int i = 0; i < 1; i++) {
             Order order = orders.get(i);
+            //asyncCreateOrder(order);
             Sitting sitting = order.sitting;
             Table table = sitting.restaurantTable;
             Log.d("ORDER ID", "" + order.id);
@@ -247,111 +271,5 @@ public class MainActivity extends AppCompatActivity {
         // Your UI code
         Log.d("DISH", "Size: " +dishes.size());
     }
-
-    // Methods used for testing below
-    /*
-    private void createOrder() {
-        Order first = new Order();
-        first.note = "Extra salt";
-        first.tableId = 3;
-
-        // First order item and its category
-        Order.OrderItem item = new Order.OrderItem();
-        item.dishId = 1;    // id of dish ordered?
-        item.category = 2;  // appetizer, main course, dessert?
-
-
-        // Second order item and its category
-        Order.OrderItem item2 = new Order.OrderItem();
-        item2.dishId = 1;
-        item2.category = 2;
-
-        //Third order item and its category
-        Order.OrderItem item3 = new Order.OrderItem();
-        item3.dishId = 2;
-        item3.category = 2;
-
-        //Creates a list of the items ordered
-        first.orderedItems = java.util.Arrays.asList(item, item2, item3);
-
-        //asynchronous post the order to the json-server ( later database)
-        ordersRepo.postOrder(first, new OrdersRepository.PostCallback() {
-            @Override
-            public void onSuccess(Order postOrder) {
-                Log.d("ORDER", "SUCCESSFULLY POSTED ORDER");
-            }
-            @Override
-            public void onError(String message) {
-                Log.e("ORDER", "FAILED TO POST: " + message);
-            }
-        });
-    }
-    private void createOrder2() {
-        Order first = new Order();
-        first.note = "Ingen lök";
-        first.tableId = 5;
-
-        // First order item and its category
-        Order.OrderItem item = new Order.OrderItem();
-        item.dishId = 3;    // id of dish ordered?
-        item.category = 1;  // appetizer, main course, dessert?
-
-        // Second order item and its category
-        Order.OrderItem item2 = new Order.OrderItem();
-        item2.dishId = 5;
-        item2.category = 2;
-
-        //Third order item and its category
-        Order.OrderItem item3 = new Order.OrderItem();
-        item3.dishId = 4;
-        item3.category = 2;
-        first.orderedItems = java.util.Arrays.asList(item, item2, item3);
-
-        //asynchronous post the order to the json-server ( later database)
-        ordersRepo.postOrder(first, new OrdersRepository.PostCallback() {
-            @Override
-            public void onSuccess(Order postOrder) {
-                Log.d("ORDER", "SUCCESSFULLY POSTED ORDER");
-            }
-            @Override
-            public void onError(String message) {
-                Log.e("ORDER", "FAILED TO POST: " + message);
-            }
-        });
-    }
-    private void createOrder3() {
-        Order first = new Order();
-        first.note = "Extra allt";
-        first.tableId = 2;
-
-        // First order item and its category
-        Order.OrderItem item = new Order.OrderItem();
-        item.dishId = 2;    // id of dish ordered?
-        item.category = 2;  // appetizer, main course, dessert?
-
-        // Second order item and its category
-        Order.OrderItem item2 = new Order.OrderItem();
-        item2.dishId = 1;
-        item2.category = 2;
-
-        //Third order item and its category
-        Order.OrderItem item3 = new Order.OrderItem();
-        item3.dishId = 3;
-        item3.category = 3;
-        first.orderedItems = java.util.Arrays.asList(item, item2, item3);
-
-        //asynchronous post the order to the json-server ( later database)
-        ordersRepo.postOrder(first, new OrdersRepository.PostCallback() {
-            @Override
-            public void onSuccess(Order postOrder) {
-                Log.d("ORDER", "SUCCESSFULLY POSTED ORDER");
-            }
-            @Override
-            public void onError(String message) {
-                Log.e("ORDER", "FAILED TO POST: " + message);
-            }
-        });
-    }
-    */
 }
 
