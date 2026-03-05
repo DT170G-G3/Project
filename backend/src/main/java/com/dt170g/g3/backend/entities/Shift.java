d746e966fb6d5701c2cb41ed7839cbecf5fd7f0a
Jesper <jesper.elovsson92@gmail.com>
2026-03-05 14:33:46 +0100

    Adding basic shiftAPI

--- backend/src/main/java/com/dt170g/g3/backend/entities/Shift.java
+++ backend/src/main/java/com/dt170g/g3/backend/entities/Shift.java
@@ -21,6 +21,7 @@ public class Shift {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private LocalDate date;
 
     @ManyToOne
--- infra/db/migrations/V6__schedule_tables.sql
+++ infra/db/migrations/V6__schedule_tables.sql
@@ -1,6 +1,7 @@
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL        
+    name VARCHAR(50) NOT NULL,
+    android_id varchar(25)     
 ) ENGINE=InnoDB;
 
 CREATE TABLE shift_type (
@@ -29,19 +30,21 @@ CREATE TABLE employee_works_shift(
 
 
 -- Insert Into Employee
-INSERT INTO employee (name) VALUES
-('Kalle'),
-('Karl'),
-('Krister'),
-('Kristina'),
-('Karin'),
-('Kim'),
-('Kanelbullen'),
-('Gud'),
-('Konan Barbaren'),
-('Katjakaj'),
-('Bentebent'),
-('En Tallrik Musli');
+INSERT INTO employee (name, android_id) VALUES
+('Kalle', "cd20486bd301b60e"),
+('Sigrid' , "cd20486bd301b60d"),
+('Mike', "cd20486bd301b60f"),
+('Karl', "cd20486bd301b601"),
+('Krister', "cd20486bd301b602"),
+('Kristina', "cd20486bd301b603"),
+('Karin', "cd20486bd301b604"),
+('Kim', "cd20486bd301b605"),
+('Kanelbullen', "cd20486bd301b606"),
+('Gud', "cd20486bd301b607"),
+('Konan Barbaren', "cd20486bd301b608"),
+('Katjakaj', "cd20486bd301b609"),
+('Bentebent', "cd20486bd301b61a"),
+('En Tallrik Musli', "cd20486bd301b61e");
 
 -- Insert Into Shift Type
 INSERT INTO shift_type (name, start_time, end_time) VALUES
